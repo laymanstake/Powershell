55ccccca15de17ed199e6322a3a02238098c3b44
Nitish Kumar <93420705+laymanstake@users.noreply.github.com>
2024-07-28 21:34:23 +0530

    Update Get-EntraIDDetail.ps1

--- Get-EntraIDDetail.ps1
+++ Get-EntraIDDetail.ps1
@@ -1,8 +1,9 @@
 <#  
     Author : Nitish Kumar (nitish@nitishkumar.net)
     Performs Entra ID Assessment
-    version 1.0 | 17/07/2023 Initial version
-	version 1.1 | 19/07/2023 Error handling improvements
+    version 1.0 | 17/07/2024 Initial version
+    version 1.1 | 19/07/2024 Error handling improvements
+    version 1.2 | 28/07/2024 Application details performance improvements
 
     Disclaimer: This script is designed to only read data from the entra id and should not cause any problems or change configurations but author do not claim to be responsible for any issues. Do due dilligence before running in the production environment
 #>
